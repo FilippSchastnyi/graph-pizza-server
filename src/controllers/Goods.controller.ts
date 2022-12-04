2fc315e250d9b1533239761cd05829033d79f354
Pilip.Shchasny <c-pshchasny@neogov.net>
2022-12-04 23:37:23 +0300

    GP-SERVER-pshchasny-fixed-routes

--- src/controllers/Goods.controller.ts
+++ src/controllers/Goods.controller.ts
@@ -1,13 +1,19 @@
-class UserController {
+import goodsModel from "../models/goods.model";
+
+class GoodsController {
 
   async getAll(req, res) {
-
   }
 
-  async getOne(req, res) {
-
+  async createGoods(req, res) {
+    const name = req.body.name
+     await goodsModel.create({
+      name,
+    }).catch((e)=>{
+       console.log(e)})
+    return res.json({name})
   }
 
 }
 
-export default new UserController();
+export default new GoodsController();
--- src/models/_SQLModels.ts
+++ src/models/_SQLModels.ts
@@ -1,54 +1,39 @@
-import basketModel from "./basket.model";
-import basketPizzaModel from "./basketPizza.model";
 import goodsModel from "./goods.model";
-import pizzaModel from "./pizza.model";
-import rateModel from "./rate.model";
-import userModel from "./user.model";
-
+import goodsSpecificationModel from "./goods-specification.model";
+import goodsCategoryModel from "./goods-category.model";
+import sizeModel from "./size.model";
+import goodsSizeModel from "./goods-size.model";
 
 class SQLModels {
   constructor() {
   }
 
   initModels = async () => {
-    this.setRelations();
     this.syncTables()
-      .catch((e)=>{console.log(e)})
+      .catch((e) => {
+        console.log(e)
+      })
+    this.setRelations();
   }
 
   setRelations = (): void => {
-/*    /!** Basket Model Relations**!/
-    basketModel.belongsTo(userModel)
-    basketModel.hasOne(basketPizzaModel)
+    /** Goods Model Model Relations**/
+    goodsModel.hasOne(goodsSpecificationModel)
+    goodsSpecificationModel.belongsTo(goodsModel)
 
-    /!** BasketPizza Model Relations**!/
-    basketPizzaModel.hasMany(pizzaModel)
-    basketPizzaModel.belongsTo(basketModel)
+    goodsModel.belongsToMany(sizeModel, {through: goodsSizeModel})
+    sizeModel.belongsToMany(goodsModel, {through: goodsSizeModel})
 
-    /!** Goods Model Relations**!/
-    goodsModel.hasMany(pizzaModel)
-
-    /!** Pizza Model Relations**!/
-    pizzaModel.belongsTo(goodsModel)
-    pizzaModel.hasOne(basketPizzaModel)
-    pizzaModel.hasOne(rateModel)
-
-    /!** Rating Model Relations**!/
-    rateModel.belongsTo(userModel)
-    rateModel.hasMany(userModel)
-
-    /!** User Model Relations**!/
-    userModel.hasOne(basketModel)
-    userModel.hasMany(rateModel)*/
+    goodsModel.hasMany(goodsCategoryModel)
+    goodsCategoryModel.belongsTo(goodsModel)
   }
 
   syncTables = async () => {
-/*    await basketModel.sync()
-    await basketPizzaModel.sync()
-    await goodsModel.sync()
-    await pizzaModel.sync()
-    await rateModel.sync()*/
-    await userModel.sync()
+        await goodsModel.sync()
+        await goodsSpecificationModel.sync()
+        await sizeModel.sync()
+        await goodsCategoryModel.sync()
+        await goodsSizeModel.sync()
   }
 }
 
